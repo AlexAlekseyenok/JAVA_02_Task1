0c7164e33bd8ff3efef39001f6ce3d26ee3a95e8
Dmitry Evdokimov <vlg1evdokimov@gmail.com>
2020-07-08 21:21:30 +0300

    Initail commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ClientBalance = 2_000_000_000;
+        int Transfer = 500_000_000;
+        int TotalBalance = ClientBalance + Transfer;
+        System.out.println(TotalBalance);
+    }
+}
