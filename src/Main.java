a29025ed39d1e8f1421efc1d33b74815ea3ac9bd
Dmitry Evdokimov <vlg1evdokimov@gmail.com>
2020-07-08 23:19:55 +0300

    Rewrite README.md

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ClientBalance = 2_000_000_000;
-        int Transfer = 500_000_000;
-        int TotalBalance = ClientBalance + Transfer;
-        System.out.println(TotalBalance);
+        int clientBalance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int totalBalance = clientBalance + transfer;
+        System.out.println(totalBalance);
     }
 }
